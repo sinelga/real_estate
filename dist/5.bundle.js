86a28cfc2547dce2160e12905cae68254900570b
juno <juno@krasava.fi>
2016-01-11 07:33:48 +0200

    dev

---
+++ dist/5.bundle.js
@@ -0,0 +1,19 @@
+webpackJsonp([5],{
+
+/***/ 228:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	module.exports = {
+	  path: ':assignmentId',
+	  getComponent: function getComponent(location, cb) {
+	    __webpack_require__.e/* nsure */(6, function (require) {
+	      cb(null, __webpack_require__(229));
+	    });
+	  }
+	};
+
+/***/ }
+
+});
